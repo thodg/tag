3571c82e28b6b592f47bfbb118e1ec182ebf1d38
Thomas de Grivel <thoxdg@gmail.com>
2018-04-19 18:33:33 +0000

    rename tag_search to tag_data_search, rename tag_data variables to td, add documentation.

--- tag.c
+++ tag.c
@@ -8,8 +8,8 @@
 
 static long tag_file_size (FILE *fp);
 
-int tag_search (const char *buf, const char *tag, unsigned len,
-                unsigned start)
+int tag_data_search (const char *buf, const char *tag, unsigned len,
+                     unsigned start)
 {
   unsigned end = TAG_SEARCH_LEN - len;
   while (start < end) {
@@ -37,7 +37,7 @@ long tag_data_seek (FILE *fp)
     int start;
     if (br < TAG_START_LEN)
       return -1;
-    start = tag_search(buf, TAG_START, TAG_START_LEN, 0);
+    start = tag_data_search(buf, TAG_START, TAG_START_LEN, 0);
     if (start >= 0) {
       pos += start;
       fseek(fp, pos, SEEK_SET);
@@ -48,22 +48,23 @@ long tag_data_seek (FILE *fp)
   return -1;
 }
 
-int tag_data_read (FILE *fp, s_tag_data *tag_data)
+int tag_data_read (FILE *fp, s_tag_data *td)
 {
-  size_t br = fread(tag_data->byte, 1, TAG_SEARCH_LEN, fp);
+  size_t br = fread(td->byte, 1, TAG_SEARCH_LEN, fp);
   int end;
   if (br < TAG_START_LEN + TAG_END_LEN)
     return -1;
-  if (memcmp(TAG_START, tag_data->byte, TAG_START_LEN))
+  if (memcmp(TAG_START, td->byte, TAG_START_LEN))
     return -1;
-  end = tag_search(tag_data->byte, TAG_END, TAG_END_LEN, TAG_START_LEN);
+  end = tag_data_search(td->byte, TAG_END, TAG_END_LEN,
+                        TAG_START_LEN);
   if (end < 0)
     return -1;
-  tag_data->len = end + TAG_END_LEN;
+  td->len = end + TAG_END_LEN;
   return 0;
 }
 
-int tag_data_from_file (FILE *fp, s_tag_data *tag_data)
+int tag_data_from_file (FILE *fp, s_tag_data *td)
 {
   if (!fp)
     return -1;
@@ -74,10 +75,10 @@ int tag_data_from_file (FILE *fp, s_tag_data *tag_data)
     if (tag_data_seek(fp) < 0)
       return -1;
   }
-  return tag_data_read(fp, tag_data);
+  return tag_data_read(fp, td);
 }
 
-int tag_data_from_path (const char *path, s_tag_data *tag_data)
+int tag_data_from_path (const char *path, s_tag_data *td)
 {
   FILE *fp = fopen(path, "r");
   int result = -1;
@@ -90,72 +91,72 @@ int tag_data_from_path (const char *path, s_tag_data *tag_data)
     if (tag_data_seek(fp) < 0)
       goto end;
   }
-  result = tag_data_read(fp, tag_data);
+  result = tag_data_read(fp, td);
  end:
   fclose(fp);
   return result;
 }
 
-int tag_data_write (FILE *fp, s_tag_data *tag_data)
+int tag_data_write (FILE *fp, s_tag_data *td)
 {
-  if (tag_data->len > TAG_SEARCH_LEN)
+  if (td->len > TAG_SEARCH_LEN)
     return -1;
-  if (fwrite(tag_data->byte, tag_data->len, 1, fp) != 1)
+  if (fwrite(td->byte, td->len, 1, fp) != 1)
     return -1;
   return 0;
 }
 
-int tag_data_to_file (FILE *fp, s_tag_data *tag_data)
+int tag_data_to_file (FILE *fp, s_tag_data *td)
 {
   if (!fp)
     return -1;
   if (fseek(fp, 0, SEEK_END) < 0)
     return -1;
-  return tag_data_write(fp, tag_data);
+  return tag_data_write(fp, td);
 }
 
-int tag_data_to_path (const char *path, s_tag_data *tag_data)
+int tag_data_to_path (const char *path, s_tag_data *td)
 {
   FILE *fp = fopen(path, "a");
   int result;
   if (!fp)
     return -1;
-  result = tag_data_write(fp, tag_data);
+  result = tag_data_write(fp, td);
   fclose(fp);
   return result;
 }
 
-void tag_data_mem (s_tag_data *tag_data, const char *buf, unsigned len)
+void tag_data_mem (s_tag_data *td, const char *buf, unsigned len)
 {
-  memcpy(tag_data->byte + tag_data->len, buf, len);
-  tag_data->len += len;
+  memcpy(td->byte + td->len, buf, len);
+  td->len += len;
 }
 
-void tag_data_str (s_tag_data *tag_data, const char *str)
+void tag_data_str (s_tag_data *td, const char *str)
 {
   unsigned len = strlen(str);
-  memcpy(tag_data->byte + tag_data->len, str, len);
-  tag_data->len += len;
+  memcpy(td->byte + td->len, str, len);
+  td->len += len;
 }
 
-void tag_data_tag (s_tag_data *tag_data, const char *key,
+void tag_data_tag (s_tag_data *td, const char *key,
                    const char *value)
 {
-  tag_data_str(tag_data, " <");
-  tag_data_str(tag_data, key);
-  tag_data_str(tag_data, ">");
-  tag_data_str(tag_data, value);
-  tag_data_str(tag_data, "</");
-  tag_data_str(tag_data, key);
-  tag_data_str(tag_data, ">\n");
+  tag_data_str(td, " <");
+  tag_data_str(td, key);
+  tag_data_str(td, ">");
+  tag_data_str(td, value);
+  tag_data_str(td, "</");
+  tag_data_str(td, key);
+  tag_data_str(td, ">\n");
 }
 
-void tag_data_start (s_tag_data *tag_data)
+void tag_data_start (s_tag_data *td)
 {
-  tag_data->len = 0;
-  tag_data_mem(tag_data, TAG_START, TAG_START_LEN);
+  td->len = 0;
+  tag_data_mem(td, TAG_START, TAG_START_LEN);
 }
 
-void tag_data_end (s_tag_data *tag_data) {
-  tag_data_mem(tag_data, TAG_END, TAG_END_LEN);
+void tag_data_end (s_tag_data *td) {
+  tag_data_mem(td, TAG_END, TAG_END_LEN);
 }
--- tag.h
+++ tag.h
@@ -14,38 +14,70 @@
 #define TAG_SEARCH_LEN 2048
 
 /* 1. TAG DATA : read and write tag data region from and to file. */
-/* TAG DATA is limited to 2048 bytes including start and end markers. */
-/* Actual TAG DATA payload can thus be 2030 bytes large. */
-/* It is not a short story, only inline metadata about current file. */
+/* Size is limited to 2048 bytes including start and end markers. */
+/* Content is thus limited to 2030 bytes. Not a short story. Only */
+/* inline metadata about current file, better at start of stream. */
 
 typedef struct tag_data {
   unsigned short len;
   char byte[TAG_SEARCH_LEN];
 } s_tag_data;
 
+/* search tag in buffer */
 int tag_data_search (const char *buf, const char *tag, unsigned len,
-                unsigned start);
+                     unsigned start);
+
+/* seek forward in file until start tag is found */
 long tag_data_seek (FILE *fp);
-int tag_data_read (FILE *fp, s_tag_data *tag_data);
-int tag_data_from_file (FILE *fp, s_tag_data *tag_data);
-int tag_data_from_path (const char *path, s_tag_data *tag_data);
-int tag_data_write (FILE *fp, s_tag_data *tag_data);
-int tag_data_to_file (FILE *fp, s_tag_data *tag_data);
-int tag_data_to_path (const char *path, s_tag_data *tag_data);
 
-/* write to s_tag_data */
+/* read tag data at current position */
+int tag_data_read (FILE *fp, s_tag_data *td);
 
-void tag_data_mem (s_tag_data *tag_data, const char *buf, unsigned len);
-void tag_data_str (s_tag_data *tag_data, const char *str);
-void tag_data_start (s_tag_data *tag_data);
-void tag_data_end (s_tag_data *tag_data);
-void tag_data_tag (s_tag_data *tag_data, const char *key,
-                   const char *value);
+/* read tag data from open file starting at end */
+int tag_data_from_file (FILE *fp, s_tag_data *td);
+
+/* open file and read tag data starting at end */
+int tag_data_from_path (const char *path, s_tag_data *td);
+
+/* write tag data at current position */
+int tag_data_write (FILE *fp, s_tag_data *td);
+
+/* write tag data at end of file */
+int tag_data_to_file (FILE *fp, s_tag_data *td);
+
+/* open file and write tag data at end of file */
+int tag_data_to_path (const char *path, s_tag_data *td);
+
+/* write buffer to s_tag_data */
+void tag_data_mem (s_tag_data *td, const char *buf, unsigned len);
+
+/* write string to s_tag_data */
+void tag_data_str (s_tag_data *td, const char *str);
+
+/* write start tag to s_tag_data */
+void tag_data_start (s_tag_data *td);
+
+/* write end tag to s_tag_data */
+void tag_data_end (s_tag_data *td);
+
+/* write key value tag to s_tag_data */
+void tag_data_tag (s_tag_data *td, const char *key, const char *value);
+
+/* read next key value from s_tag_data, return position */
+int tag_data_read (s_tag_data *td, int start,
+                   char *key, unsigned key_size,
+                   char *value, unsigned value_size);
+
+/* find position of key in s_tag_data */
+int tag_data_find (s_tag_data *td, int start, const char *key);
+
+/* get value of key in s_tag_data */
+int tag_data_get (s_tag_data *td, int start, const char *key,
+                  char *value, unsigned value_size);
 
 /* 2. TAG : structure tag data region from and to actual tags */
 /* actually tag data is xml enclosed in TAG_START and TAG_END */
-/* known tags are : title, track, artist, album, year, genre. */
-/* Just use XML library to read and write tags to s_tag_data. */
+/* known tags are : artist, album, genre, track, title, year. */
 
 
 #endif /* TAG_H */
